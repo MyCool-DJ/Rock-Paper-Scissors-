70949fbe206e42d7d6d053779e352995d6fefaff
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-09 07:54:24 +0000

    formatted the code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#day14 of 100days-of-code via REplit
 from getpass import getpass as input
 
 print("Welcome to the classic game of Rock, Paper, Scissors!")
@@ -6,17 +7,25 @@ player1 = input("Player 1, enter your first turn: ")
 player2 = input("Player 2, enter your first turn: ")
 print("OK, let's see!")
 
-if (player1 == "Rock" or player1 == "rock" or player1 == "🪨") and (player2 == "Paper" or player2 == "paper" or player2 == "📄"):
-	print("Player 2 wins!!")
-elif (player1 == "Paper" or player1 == "paper" or player1 == "📄") and (player2 == "Rock" or player2 == "rock" or player2 == "🪨"):
-	print("Player 1 wins!!") 
-elif (player1 == "Rock" or player1 == "rock" or player1 == "🪨") and (player2 == "Scissors" or player2 == "scissors"):
-	print("Player 1 wins!")
-elif (player1 == "Scissors" or player1 == "scissors") and (player2 == "Rock" or player2 == "rock" or player2 == "🪨"):
-	print("Player 2 wins!")
-elif (player1 == "Paper" or player1 == "paper" or player1 == "📄") and (player2 == "Scissors" or player2 == "scissors"):
-	print("Player 2 wins!")
-elif (player1 == "Scissors" or player1 == "scissors") and (player2 == "Paper" or player2 == "paper"):
-	print("Player 1 wins!")
+if (player1 == "Rock" or player1 == "rock"
+        or player1 == "🪨") and (player2 == "Paper" or player2 == "paper"
+                                or player2 == "📄"):
+    print("Player 2 wins!!")
+elif (player1 == "Paper" or player1 == "paper" or player1
+      == "📄") and (player2 == "Rock" or player2 == "rock" or player2 == "🪨"):
+    print("Player 1 wins!!")
+elif (player1 == "Rock" or player1 == "rock"
+      or player1 == "🪨") and (player2 == "Scissors" or player2 == "scissors"):
+    print("Player 1 wins!")
+elif (player1 == "Scissors"
+      or player1 == "scissors") and (player2 == "Rock" or player2 == "rock"
+                                     or player2 == "🪨"):
+    print("Player 2 wins!")
+elif (player1 == "Paper" or player1 == "paper"
+      or player1 == "📄") and (player2 == "Scissors" or player2 == "scissors"):
+    print("Player 2 wins!")
+elif (player1 == "Scissors"
+      or player1 == "scissors") and (player2 == "Paper" or player2 == "paper"):
+    print("Player 1 wins!")
 else:
-	print("Draw!")
+    print("Draw!")
